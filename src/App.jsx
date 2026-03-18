02b72d59436cc827572df9be1c3aed086fb821e1
yunabin <yhb1020525@gmail.com>
2026-03-18 14:41:05 +0900

    4

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,22 @@ import { useState } from 'react'
 function App() {
     const [todos, setTodos] = useState(['할일1', '할일2', '할일3'])
 
+    const handleOnSubmit = (e) => {
+        e.preventDefault() // 새로고침을 사전에 방지하는 것
+        const form = e.target
+        if (form.todo.value.length == 0) {
+            alert('할 일을 입력해 주세요!')
+            return
+        }
+        setTodos([...todos, form.todo.value])
+    }
+
     return (
         <>
+            <form onSubmit={handleOnSubmit}>
+                <input type="text" name="todo" />
+                <button>등록</button>
+            </form>
             <ul>
                 {todos.map((item, i) => (
                     <li key={i}>{item}</li>
