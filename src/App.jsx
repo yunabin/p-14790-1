e02d77b137415516a0e870a30dd8d36b0968f2ce
yunabin <yhb1020525@gmail.com>
2026-03-18 14:30:25 +0900

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { useState } from 'react'
+
 function App() {
-    return <>hello world</>
+    const [todos, setTodos] = useState(['할일1', '할일2', '할일3'])
+
+    return (
+        <>
+            <ul>
+                {todos.map((item, i) => (
+                    <li key={i}>{item}</li>
+                ))}
+            </ul>
+        </>
+    )
 }
 
 export default App
