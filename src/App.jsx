9abf81b867f227dfd62f1b3fdbeb646f98d9d608
yunabin <yhb1020525@gmail.com>
2026-03-18 15:26:05 +0900

    6

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from 'react'
 
 function App() {
-    const [todos, setTodos] = useState(['할일1', '할일2', '할일3'])
+    const [todos, setTodos] = useState([
+        { todo: '할일1', completed: true },
+        { todo: '할일2', completed: true },
+        { todo: '할일3', completed: false },
+    ])
 
     const handleOnSubmit = (e) => {
         e.preventDefault() // 새로고침을 사전에 방지하는 것
@@ -10,7 +14,17 @@ function App() {
             alert('할 일을 입력해 주세요!')
             return
         }
-        setTodos([...todos, form.todo.value])
+        setTodos([...todos, { todo: form.todo.value, completed: false }])
+    }
+
+    const deleteTodo = (selectedIndex) => {
+        const nextState = todos.filter((item, i) => i !== selectedIndex)
+        setTodos(nextState)
+    }
+
+    const toggleTodo = (selectedIndex) => {
+        const nextState = todos.map((item, i) => (i == selectedIndex ? { ...item, completed: !item.completed } : item)) // 아닐 때는 원본 데이터 그대로 둬라
+        setTodos(nextState)
     }
 
     return (
@@ -21,7 +35,12 @@ function App() {
             </form>
             <ul>
                 {todos.map((item, i) => (
-                    <li key={i}>{item}</li>
+                    <li key={i}>
+                        {JSON.stringify(item.completed)}
+                        <input type="checkbox" checked={item.completed} onChange={() => toggleTodo(i)} />
+                        <span>{item.todo}</span>
+                        <button onClick={() => deleteTodo(i)}>X</button>
+                    </li>
                 ))}
             </ul>
         </>
