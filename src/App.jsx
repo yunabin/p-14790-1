7358ad9513d5d527356beccff84ac6a27ad50aad
yunabin <yhb1020525@gmail.com>
2026-03-18 15:37:15 +0900

    7

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 function App() {
     const [todos, setTodos] = useState([
-        { todo: '할일1', completed: true },
-        { todo: '할일2', completed: true },
-        { todo: '할일3', completed: false },
+        { id: 1, todo: '할일1', completed: true },
+        { id: 2, todo: '할일2', completed: true },
+        { id: 3, todo: '할일3', completed: false },
     ])
+    let lastId = useRef(4)
+    //초기화값을 4로 설정
 
     const handleOnSubmit = (e) => {
         e.preventDefault() // 새로고침을 사전에 방지하는 것
@@ -14,16 +16,17 @@ function App() {
             alert('할 일을 입력해 주세요!')
             return
         }
-        setTodos([...todos, { todo: form.todo.value, completed: false }])
+        setTodos([...todos, { id: lastId.current, todo: form.todo.value, completed: false }])
+        lastId.current++
     }
 
-    const deleteTodo = (selectedIndex) => {
-        const nextState = todos.filter((item, i) => i !== selectedIndex)
+    const deleteTodo = (selectedID) => {
+        const nextState = todos.filter((item) => item.id !== selectedID)
         setTodos(nextState)
     }
 
-    const toggleTodo = (selectedIndex) => {
-        const nextState = todos.map((item, i) => (i == selectedIndex ? { ...item, completed: !item.completed } : item)) // 아닐 때는 원본 데이터 그대로 둬라
+    const toggleTodo = (selectedID) => {
+        const nextState = todos.map((item) => (item.id == selectedID ? { ...item, completed: !item.completed } : item)) // 아닐 때는 원본 데이터 그대로 둬라
         setTodos(nextState)
     }
 
@@ -34,12 +37,14 @@ function App() {
                 <button>등록</button>
             </form>
             <ul>
-                {todos.map((item, i) => (
-                    <li key={i}>
+                {todos.map((item) => (
+                    <li key={item.id}>
                         {JSON.stringify(item.completed)}
-                        <input type="checkbox" checked={item.completed} onChange={() => toggleTodo(i)} />
-                        <span>{item.todo}</span>
-                        <button onClick={() => deleteTodo(i)}>X</button>
+                        <input type="checkbox" checked={item.completed} onChange={() => toggleTodo(item.id)} />
+                        <span>
+                            {item.id} / {item.todo}
+                        </span>
+                        <button onClick={() => deleteTodo(item.id)}>X</button>
                     </li>
                 ))}
             </ul>
